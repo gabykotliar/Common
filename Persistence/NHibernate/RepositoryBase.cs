38e7bd1af89db6637bd1b5ed27e7e879caccee2e
matiasurrutia <matias.urrutia@hotmail.com>
2012-11-30 17:49:37 -0300

    Adding NHibernate Registry and SessionModule for Persistence Implementation

--- Persistence/NHibernate/RepositoryBase.cs
+++ Persistence/NHibernate/RepositoryBase.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Common.Persistence;
 using NHibernate;
 using NHibernate.Criterion;
 
---
+++ Persistence/NHibernate/SessionModule.cs
@@ -0,0 +1,64 @@
+﻿namespace Common.Persistence.NHibernate
+{
+    using Common;
+    using global::NHibernate;
+    using StructureMap;
+    using System;
+    using System.Linq;
+    using System.Web;
+
+    public class SessionModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.EndRequest += this.ContextEndRequest;
+        }
+
+        public void Dispose()
+        {            
+        }
+
+        public void ContextEndRequest(Object sender, EventArgs e)
+        {
+            var request = ((HttpApplication)sender).Request;
+
+            if (!RequestRequiresSessionUpdate(request)) return;
+
+            try
+            {
+                FlushSession();
+            }
+            catch (Exception ex)
+            {
+                AppServices.Log.Exception(ex).WithMessage("NHibernate Session Module. Error trying to flush the session.");
+            }
+        }
+
+        private static bool RequestRequiresSessionUpdate(HttpRequest request)
+        {
+            var safeMethods = new[] { "HEAD", "GET", "OPTIONS", "TRACE" };
+
+            var isSafe = safeMethods.Contains(request.HttpMethod);
+
+            return !isSafe;
+        }
+
+        private static void FlushSession()
+        {
+            var session = ObjectFactory.GetInstance<ISession>();
+            
+            if (session == null) return;            
+
+            if (session.Transaction != null && session.Transaction.IsActive)
+            {
+                session.Transaction.Rollback();
+            }
+            else
+            {
+                session.Flush();
+            }
+
+            session.Close();
+        }
+    }
+}
