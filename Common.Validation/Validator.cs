f31ce1ef4cb0dd261867e4bb42edf75d0d8232ad
gabykotliar <gabykotliar@gmail.com>
2013-03-07 16:42:04 -0300

    validation

---
+++ Common.Validation/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Common.Validation
+{
+    public abstract class Validator
+    {
+        // TODO: el problema es que si tengo esto en un assembly separado de common no lo puedo poner en appservices.
+        // y si lo meto en common todos van a tener que agregar referencias a DataAnnotations.
+
+        public abstract ICollection<ValidationResult> Validate(object instance);
+
+        public void ValidateAndThrow(object instance)
+        {
+            var error = Validate(instance).FirstOrDefault();
+
+            if (error != null)
+                throw new ValidationException(error.ErrorMessage);
+        }
+
+        public bool IsValid(object instance)
+        {
+            return !Validate(instance).Any();
+        }
+    }
+}
