a0f7278f59d9b74025da0e6945b1158c50cefbbe
gabykotliar <gabykotliar@gmail.com>
2013-07-11 15:27:55 -0300

    RoutInfo throws an exception on bad url format. Test improvement

---
+++ Tests/Common/RouteData/Given_a_url_for_routing/WhenParsingWithRouteInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Common.Web.Mvc.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Common.RouteData.Given_a_url_for_routing
+{
+    [TestClass]
+    public class WhenParsingWithRouteInfo
+    {
+        [ClassInitialize]
+        public static void SetupTest(TestContext context)
+        {
+            SetRoutes();
+        }
+
+        private static void SetRoutes()
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), AllowDerivedTypes = false)]
+        public void InvalidUrlShouldThrowAnException()
+        {
+            RouteInfo.GetRouteDataByUrl(@"error/notFound404");
+        }
+
+        [TestMethod]
+        public void ValuesShouldBeParsedCorrectly()
+        {
+            var routeData = RouteInfo.GetRouteDataByUrl(@"~/error/notFound404");
+
+            Assert.AreEqual(routeData.Values["controller"], "error");
+            Assert.AreEqual(routeData.Values["action"], "notFound404");
+        }
+    }
+}
--- Web/MVC/Routing/RouteInfo.cs
+++ Web/MVC/Routing/RouteInfo.cs
@@ -1,12 +1,28 @@
-﻿using System.Web.Routing;
+﻿using System;
+using System.Web.Routing;
 
 namespace Common.Web.Mvc.Routing
 {
     public class RouteInfo
     {
-        public static System.Web.Routing.RouteData GetRouteDataByUrl(string url)
+        /// <summary>
+        /// Creates route data based on a url expressed as a string.
+        /// </summary>
+        /// <param name="appRelativeUrl">
+        /// The relative path of the route you want to parse as to be provided to <see cref="System.Web.HttpRequestBase.AppRelativeCurrentExecutionFilePath"/>.
+        /// 
+        /// As on documentation:
+        /// Summary:
+        /// When overridden in a derived class, gets the virtual path of the application root and makes it relative by using the tilde (~) notation for the application root (as in "~/page.aspx").
+        /// 
+        /// Returns:
+        /// The virtual path of the application root for the current request with the tilde operator (~) added.
+        /// </param>
+        public static RouteData GetRouteDataByUrl(string appRelativeUrl)
         {
-            return RouteTable.Routes.GetRouteData(new RewritedHttpContextBase(url));
+            if (!appRelativeUrl.StartsWith(@"~/")) throw new ArgumentException("The value of the specified URL is invalid. It should start with '~/'as it uses System.Web.HttpRequestBase.AppRelativeCurrentExecutionFilePath. Please check the documentation for that property ", "appRelativeUrl");
+
+            return RouteTable.Routes.GetRouteData(new RewritedHttpContextBase(appRelativeUrl));
         }
     }
 }
